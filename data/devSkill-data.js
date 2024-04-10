32b9c19bbd107250d611b010f3981c48d4b3ccca
kiibra <kibra99@gmail.com>
2024-04-10 18:48:00 -0400

    create data directory and update data

---
+++ data/devSkill-data.js
@@ -0,0 +1,11 @@
+const devSkills = [
+  {skill: 'Front-end', proficiency: true, _id: 2387 }
+  {skill: 'Back-end', proficiency: true, _id: 2209}
+  {skill: 'UI/UX', proficiency: true, _id: 9852}
+  {skill: IT, proficiency: true, _id: 3984}
+]
+
+
+export {
+  devSkills
+}

2f8082613fe591ad9a6f8a9073bd2482255d635c
kiibra <kibra99@gmail.com>
2024-04-10 18:49:34 -0400

    update array of data

--- data/devSkill-data.js
+++ data/devSkill-data.js
@@ -1,8 +1,8 @@
 const devSkills = [
-  {skill: 'Front-end', proficiency: true, _id: 2387 }
-  {skill: 'Back-end', proficiency: true, _id: 2209}
-  {skill: 'UI/UX', proficiency: true, _id: 9852}
-  {skill: IT, proficiency: true, _id: 3984}
+  {skill: 'Front-end', proficiency: true, _id: 2387 },
+  {skill: 'Back-end', proficiency: true, _id: 2209},
+  {skill: 'UI/UX', proficiency: true, _id: 9852},
+  {skill: IT, proficiency: true, _id: 3984},
 ]
 
 

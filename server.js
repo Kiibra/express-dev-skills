cf6046872bfdf31766e7ae1eee716ce378862927
kiibra <kibra99@gmail.com>
2024-04-10 20:13:16 -0400

    complete configuring mongoose in a module

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // import npm packages
+import "dotenv/config.js"
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import createError from 'http-errors'
 import logger from 'morgan'
+
+import './config/database.js'
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
